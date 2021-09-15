3cf89d8a5be25869543495971c9b605de10cae4a
ilkay Polat <ilkaypolat1985@gmail.com>
2021-09-15 15:40:09 +0200

    Initial commit

---
+++ src/CategoryList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+
+class CategoryList extends Component {
+    state = {
+        categories: [{ categoryId: 1, categoryName: "Beverages" }, { categoryId: 2, categoryName: "Condiments" }]
+    };
+    render() {
+        return (
+            <div>
+                <h3>{this.props.info.title}</h3>
+               <ListGroup>
+                {
+                    this.state.categories.map(category => (
+                        <ListGroupItem id={category.categoryId} key={category.categoryId}>{category.categoryName}</ListGroupItem>
+                    ))
+                }
+               </ListGroup>
+            </div>
+        );
+    }
+}
+
+export default CategoryList;
